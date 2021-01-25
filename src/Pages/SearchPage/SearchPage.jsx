40c10727495503c8e3b9e45ebd02690bf6af77a9
Razvan Timis <timisrazvanvasile@gmail.com>
2021-01-25 21:04:21 +0200

    Add Empty search page

---
+++ src/Pages/SearchPage/SearchPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// Ce se tine pe state?
+// Ce vine pe props?
+
+export class SearchPage extends React.Component {
+
+  render() {
+    return (<div>search page</div>)
+  }
+}
