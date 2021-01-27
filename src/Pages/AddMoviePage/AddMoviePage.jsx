047c639e7aac57c220b9f379bb7ea207f4896398
tudistef <tudistef@gmail.com>
2021-01-27 20:20:20 +0200

    add movie

--- src/Pages/AddMoviePage/AddMoviePage.jsx
+++ src/Pages/AddMoviePage/AddMoviePage.jsx
@@ -1,14 +1,14 @@
-//header
 //user page after login
 //movie list
 //add movie functionality:
 //- add movie button
 //- add fileds:
-//      - poster
+//      - poster(picture)
 //      - title
 //      - year
 //      - imdbID
-//      - type
+//      - type(movie)
+//- submit
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -22,6 +22,3 @@ const AddMoviePage = () => ()
 );
 
 export default AddMoviePage;
-
-
-
