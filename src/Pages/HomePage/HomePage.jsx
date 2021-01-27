2a62863726e34ce450b0304cfa4cb8e446111ad0
bondastefana <71835217+bondastefana@users.noreply.github.com>
2021-01-27 20:59:23 +0200

    create Header

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 export class HomePage extends React.Component {
+  // Movies comp
   // slogan
-  // Carrousel component = movie categry title + slider with the movies from that category
+  // Carousel component = movie category title + slider with the movies from that category
   // Using few categories, the component will be instantiated
   // category=[category string array]
   // map(category)
-  // for each movie category => fetch(category) inside Carrousel comp => set the title with categ name;
+  // for each movie category => fetch(category) inside Carousel comp => set the title with categ name;
   // response will be passed down as props on the slider
   render() {
     return <div>home page</div>
--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Header.css'
 import { Link } from 'react-router-dom'
 import { ReactComponent as Logo } from '../../assets/AppLogo.svg'
-import { InputGroup, FormControl } from 'react-bootstrap'
+import { InputGroup } from 'react-bootstrap'
 
 export class Header extends React.Component {
   // header that renders: app-logo and app-name;
@@ -24,12 +24,9 @@ export class Header extends React.Component {
           </nav>
           <InputGroup>
             <div className="search">
-              {/* event on enter/on keydown verified, it leads you to the search page where the get mvie by name call is triggered. 
-						send the information from search input and get it on search page through state params?*/}
-              <FormControl
-                aria-label="Default"
-                aria-describedby="inputGroup-sizing-default"
-              />
+              {/* event on enter/on keydown verified, it leads you to the search page where the get movie by name call is triggered. 
+						send the information from search input and get it on search page through url*/}
+              <input placeholder="Search" type="text" />
             </div>
           </InputGroup>
         </div>
