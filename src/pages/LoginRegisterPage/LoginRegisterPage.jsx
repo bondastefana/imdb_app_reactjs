15250012accb9028a1bd846e2fadae58490aff7b
Cojocaru Ionut Liviu <cojocaruionutliviu@gmail.com>
2021-02-01 23:16:50 +0200

    Add toggle between form functionality

--- src/pages/LoginRegisterPage/LoginRegisterPage.jsx
+++ src/pages/LoginRegisterPage/LoginRegisterPage.jsx
@@ -1,68 +1,84 @@
-import React from 'react';
+import React from 'react'
 import UserIcon from "../../assets/UserIcon.svg";
 import PasswordIcon from "../../assets/PasswordIcon.svg";
 import Btn from "../../assets/Btn.svg";
-import BtnReg from "../../assets/BtnReg.svg";
 import BtnBlank from "../../assets/BtnBlank.svg";
+import BtnReg from "../../assets/BtnReg.svg";
 import BtnBlankReg from "../../assets/BtnBlankReg.svg";
-import EmailIcon from "../../assets/EmailIcon.svg";
 import './LoginRegisterPage.css'
 
-class Login extends React.Component {
+class LoginPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            data: [],
             username: '',
             password: '',
-        }
+            accessToken: '',
+            authenticated: false
+        };
+        this.handleChangeUsername = this.handleChangeUsername.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
+    }
+
+    handleChangeUsername(event) {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    handleChangePassword(event) {
+        this.setState({
+            password: event.target.value
+        })
     }
 
     handleClick(event) {
-        var apiBaseUrl = "https://movies-app-siit.herokuapp.com/auth/";
-        var payload = {
-            "email": this.username,
-            "password": this.password
-        }
-        fetch(`${apiBaseUrl}login`, payload)
-            .then(function (response) {
-                console.log(response);
-                if (response.data.code == 200) {
-                    console.log("Login successfull");
-                }
-                else if (response.data.code == 204) {
-                    console.log("Username password do not match");
-                    alert("username password do not match")
-                }
-                else {
-                    console.log("Username does not exists");
-                    alert("Username does not exist");
-                }
+        var apiBaseUrl = "https://movies-app-siit.herokuapp.com/auth/"
+        const payload = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            },
+            body: JSON.stringify({
+                'username': this.state.username,
+                'password': this.state.password
             })
-            .catch(function (error) {
-                console.log(error);
-            });
+        };
+
+        fetch(apiBaseUrl + 'login', payload)
+            .then((response) => {
+                if (response.status === 401) {
+                    return console.log("user not found/wrong password")
+                } else if (response.status === 200) {
+                    return console.log("Logged In")
+                }
+                else return response.json()
+            })
+            // .then(data => this.setItem({
+            //     data: data,
+            //     authenticated: true,
+            //     accessToken: data.accessToken
+            // }))
+            .catch((err) => console.log(err));;
     }
 
     render() {
         return (
-            <div className="base-container">
-                <div className="login-header">
-                    <img src={Btn} />
-                    <img src={BtnBlank} />
-                </div>
+            <div>
                 <div className="content">
                     <div className="login-form">
-                        <div className="user-group">
+                        <div className="userData">
                             <span>
                                 <img src={UserIcon} />
                             </span>
-                            <input type="text" name="username" placeholder="Username" onChange={(event, newValue) => this.setState({ username: newValue })} />
+                            <input type="text" name="username" placeholder="Username" value={this.state.username} onChange={this.handleChangeUsername} />
                         </div>
-                        <div className="password-group">
+                        <div className="userData">
                             <span>
                                 <img src={PasswordIcon} />
                             </span>
-                            <input type="password" name="password" placeholder="Password" onChange={(event, newValue) => this.setState({ password: newValue })} />
+                            <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChangePassword} />
                         </div>
                     </div>
                 </div>
@@ -74,71 +90,83 @@ class Login extends React.Component {
     }
 }
 
-class Register extends React.Component {
+class RegisterPage extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            data: [],
+            username: '',
+            password: '',
+            authenticated: false
+        };
+        this.handleChangeUsername = this.handleChangeUsername.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
+    }
+
+    handleChangeUsername(event) {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    handleChangePassword(event) {
+        this.setState({
+            password: event.target.value
+        })
     }
 
     handleClick(event) {
-        var apiBaseUrl = "http://https://movies-app-siit.herokuapp.com/auth/";
-        console.log("values", this.state.first_name, this.state.password);
-        var self = this;
-        var payload = {
-            "username": this.state.first_name,
-            "password": this.state.password
-        }
-        fetch(`${apiBaseUrl}register`, payload)
-            .then(function (response) {
-                console.log(response);
-                if (response.data.code == 200) {
-                    console.log("Registered succefull");
-                }
-                else if (response.data.code == 204) {
-                    console.log("Username password do not match");
-                    alert("username password do not match")
-                }
-                else {
-                    console.log("Username does not exists");
-                    alert("Username does not exist");
-                }
+        var apiBaseUrl = "https://movies-app-siit.herokuapp.com/auth/"
+        const payload = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            },
+            body: JSON.stringify({
+                'username': this.state.username,
+                'password': this.state.password
             })
-            .catch(function (error) {
-                console.log(error);
-            });
+        };
+
+        fetch(apiBaseUrl + 'register', payload)
+            .then((response) => {
+                if (response.status === 409) {
+                    return console.log("Username already existing")
+                } else if (response.status === 200) {
+                    return console.log("Registered")
+                }
+                else return response.json()
+            })
+            // .then(data => this.setItem({
+            //     data: data,
+            //     authenticated: true,
+            //     accessToken: data.accessToken
+            // }))
+            .catch((err) => console.log(err));
     }
 
     render() {
         return (
-            <div className="base-container">
-                <div className="register-header">
-                    <img src={BtnReg} />
-                    <img src={BtnBlankReg} />
-                </div>
+            <div>
                 <div className="content">
                     <div className="register-form">
                         <div className="userData">
                             <span>
                                 <img src={UserIcon} />
                             </span>
-                            <input onChange={(event, newValue) => this.setState({ username: newValue })} type="text" name="username" placeholder="Username" />
+                            <input type="text" name="username" placeholder="Username" value={this.state.username} onChange={this.handleChangeUsername} />
                         </div>
                         <div className="userData">
                             <span>
                                 <img src={PasswordIcon} />
                             </span>
-                            <input onChange={(event, newValue) => this.setState({ password: newValue })} type="password" name="password" placeholder="Password" />
+                            <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChangePassword} />
                         </div>
                         <div className="userData">
                             <span>
                                 <img src={PasswordIcon} />
                             </span>
-                            <input type="password" name="confirmPassword" placeholder="Confirm Password" />
-                        </div>
-                        <div className="userData">
-                            <span>
-                                <img src={EmailIcon} />
-                            </span>
-                            <input onChange={(event, newValue) => this.setState({ email: newValue })} type="text" name="email" placeholder="email@movieDB.com" />
+                            <input type="password" name="confirmPassword" placeholder="Confirm Password" required />
                         </div>
                     </div>
                 </div>
@@ -153,19 +181,37 @@ class Register extends React.Component {
 export class LoginRegisterPage extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            active: true,
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
 
+    handleClick(event) {
+        this.setState({
+            active: !this.state.active
+        });
     }
 
     render() {
         return (
-            <div className="LoginRegisterPage">
-                <div>
-                    <div className="container">
-                        <Login />
-                        <Register />
-                    </div>
+            <div className="base-container" >
+                <div classsName='toggle-nav'>
+                    {this.state.active ?
+                        <div>
+                            <img type='button' src={Btn} onClick={this.handleClick} />
+                            <img type='button' src={BtnBlank} onClick={this.handleClick} />
+                        </div> :
+                        <div>
+                            <img type='button' src={BtnReg} onClick={this.handleClick} />
+                            <img type='button' src={BtnBlankReg} onClick={this.handleClick} />
+                        </div>
+                    }
+                </div>
+                <div className="login-register">
+                    {this.state.active ? <RegisterPage /> : <LoginPage />}
                 </div>
             </div>
-        );
+        )
     }
 }
