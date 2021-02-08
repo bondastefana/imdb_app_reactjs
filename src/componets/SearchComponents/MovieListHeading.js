7baaddcdab05e53eaf677019b2509466f4033298
baciladan <baciladan0@gmail.com>
2021-02-08 17:30:51 +0200

    Stiling and small modifications search page

--- src/componets/SearchComponents/MovieListHeading.js
+++ src/componets/SearchComponents/MovieListHeading.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MovieListHeading = (props) => {
     return (
-        <div className='col movieHeading'>
+        <div className='movieHeading'>
             <h1>{props.heading}</h1>
         </div>
     )
