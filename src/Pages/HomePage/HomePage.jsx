f69e813bbd47027f36d1f49c36dd08632958984f
bondastefana <71835217+bondastefana@users.noreply.github.com>
2021-02-08 16:30:10 +0200

    code_review

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -21,16 +21,8 @@ export class HomePage extends React.Component {
       ],
     }
   }
-  // Movies comp
-  // slogan
-  // Carousel component = movie category title + slider with the movies from that category
-  // Using few categories, the component will be instantiated
-  // category=[category string array]
-  // map(category)
-  // for each movie category => fetch(category) inside Carousel comp => set the title with categ name;
-  // response will be passed down as props on the slider
+
   render() {
-    console.log('home')
     return (
       <Container fluid>
         <p className="motto">... bringing Joy through Reel</p>
